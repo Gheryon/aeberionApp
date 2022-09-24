3428c6295354a27288c0846b7b3b91a3a25d563e
Fco José <95536544+Gheryon@users.noreply.github.com>
2022-09-24 18:45:23 +0200

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    rellenar_menu_especies();
+
+    function rellenar_menu_especies() {
+        funcion="menu_especies";
+        $.post('../controlador/especiesController.php', {funcion}, (response)=>{
+            console.log(response);
+            const especies= JSON.parse(response);
+            let template='';
+            especies.forEach(especie => {
+                template+=`
+                <li class="nav-item">
+                  <a href="especies.php?id_especie=${especie.id}" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>${especie.nombre}</p>
+                  </a>
+                </li>
+                `;
+            });
+            $('#menu_especies').html(template);
+        })
+    }
+});
